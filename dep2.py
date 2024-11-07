aa62a592c352625ed11ffad22fbc28c2a401db98
Gopi Trinadh <gopitrinadhn@gmail.com>
2024-11-07 18:06:49 +0530

    added new show  statement

--- dep2.py
+++ dep2.py
@@ -2,13 +2,23 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName('local file').getOrCreate()
 
-data = [(1,'goopi',123)]
+# data = [(1,'goopi',123)]
+#
+# columns =['id','name','cell no']
+#
+# df = spark.createDataFrame(data , columns)
+#
+# df.show()
 
-columns =['id','name','cell no']
+data = [(1,'gopi',12),(2,"suresh",23),(3,'naresh',56)]
+
+columns = ['id','empname','section']
 
 df = spark.createDataFrame(data , columns)
 
 df.show()
+
+
 
 
 

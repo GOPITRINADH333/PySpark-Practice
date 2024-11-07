d4fe992642b5a4b12c2015fc4e01da70a0c460f3
Gopi Trinadh <gopitrinadhn@gmail.com>
2024-11-07 09:29:17 +0530

    fixed code error

--- dep3.py
+++ dep3.py
@@ -1,13 +1,13 @@
-from spark.sql import sparkSession
+from pyspark.sql import SparkSession
 
 
-spark = sparkSession.builder.appName('gopi').getorCreate
+spark = SparkSession.builder.appName('gopi').getOrCreate()
 
 data = [(1,'gopi',55),(2,'haresh',66),(3,'bhargav',77)]
 
 columns = ['id','empname','income']
 
-df = spark.createDataFrame(data , column)
+df = spark.createDataFrame(data , columns)
 
 df.show()
 
